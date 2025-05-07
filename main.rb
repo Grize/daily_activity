f97bf08f6af8351d35f71b03ff631c37d4ae7a37
Pavel <pavel@barnash.dev>
2025-05-08 00:06:54 +0300

    Create first endpoint

--- main.rb
+++ main.rb
@@ -5,3 +5,11 @@ require 'sinatra'
 get '/' do
   puts 'Hello world!'
 end
+
+post 'day' do
+  request.body.rewind
+  params = JSON.parse(request.body.read)
+  record = Services::Days::Create.new(params).call
+
+  redirect to("day/#{record.id}")
+end
